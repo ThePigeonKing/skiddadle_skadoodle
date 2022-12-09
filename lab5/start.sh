c50a0299746d7435346a12cc16947b115e19161e
ThePigeonKing <thepigeonking@mail.ru>
2022-12-10 01:06:52 +0300

    lab5 trashed?

--- lab5/start.sh
+++ lab5/start.sh
@@ -5,7 +5,7 @@ rm trace.txt
 for threads in {1..12}
 do
     echo -e "\nRUNNING ON THREADS $threads"
-    for seed in {1000..800..-7}
+    for seed in {1000..300..-7}
     do
         echo -n "."
         mpicc lab5.cpp -o lab5.out -D RANDOM_SEED=$seed
